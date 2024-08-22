90b76b202ceea4cb17db3142d945b4f4085bedd2
Vu Dang Hoang <Hoang.Vu3@metropolia.fi>
2024-08-22 11:22:53 +0300

    Second Commit.

--- 1.py
+++ 1.py
@@ -1,3 +1,17 @@
+num1 = int(input("enter firt number:"))
+num2 =int(input("enter second number:"))
+sum = num1 + num2
+print(sum)
+
+name = input("Enter your name:")
+school= input("Enter your school:")
+print("You are",name, "and your school is",school)
+
+print(f" Your name is, {name}, and your school name is :{school}")
+floatvariable = 3.486858
+print(f"Your name is,{name}, and your floatvariable is,{floatvariable}")
+
+'''
 intvariable = 3
 floatvariable = 3.14
 stringvariable = "Maybe"
@@ -5,3 +19,12 @@ stringvariable = "Maybe"
 print(intvariable)
 print(floatvariable)
 print(stringvariable)
+
+#typecasting
+intvariable = int(floatvariable)
+print("here is the int version of float variable",intvariable)
+
+shareofloan= 500.50/3
+print(shareofloan)
+print(int(shareofloan))
+'''
